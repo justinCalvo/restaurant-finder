4e8783473908d0316cc575fbfdabcf58207ad6dd
Justin Calvo <justin_calvo@hotmail.com>
2020-07-16 16:53:18 -0400

    Added ability to swipe up and down to view description

--- RestaurantProject/src/components/Description.js
+++ RestaurantProject/src/components/Description.js
@@ -1,45 +1,81 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from 'react-native-gesture-handler';
 
 const Description = ({ placeDetails, index }) => {
-  console.log(placeDetails[index]);
+  const [showDescription, setShowDescription] = useState(true);
+
   return (
-    <View>
-      <Text>Phone#: {placeDetails[index].formatted_phone_number}</Text>
-      <Text>{placeDetails[index].website}</Text>
-      <Text>
-        {placeDetails[index].opening_hours.weekday_text[0]}
-        {'\n'}
-        {placeDetails[index].opening_hours.weekday_text[1]}
-        {'\n'}
-        {placeDetails[index].opening_hours.weekday_text[2]}
-        {'\n'}
-        {placeDetails[index].opening_hours.weekday_text[3]}
-        {'\n'}
-        {placeDetails[index].opening_hours.weekday_text[4]}
-        {'\n'}
-        {placeDetails[index].opening_hours.weekday_text[5]}
-        {'\n'}
-        {placeDetails[index].opening_hours.weekday_text[6]}
-      </Text>
-      {placeDetails[index].reviews ? (
-        <FlatList
-          data={placeDetails[index].reviews}
-          keyExtractor={(item, i) => i.toString()}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.author_name}</Text>
-              <Text>Customer Rating: {item.rating}</Text>
-              <Text>{item.relative_time_description}</Text>
-              <Text>{item.text}</Text>
-            </View>
-          )}
-        />
-      ) : (
-        <Text>No Reviews</Text>
-      )}
-    </View>
+    <FlingGestureHandler
+      direction={Directions.UP}
+      onHandlerStateChange={({ nativeEvent }) => {
+        if (nativeEvent.state === State.ACTIVE) {
+          console.log('up');
+          setShowDescription(true);
+        }
+      }}>
+      <FlingGestureHandler
+        direction={Directions.DOWN}
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.ACTIVE) {
+            console.log('down');
+            setShowDescription(false);
+          }
+        }}>
+        <View>
+          <Text>Description</Text>
+          <View style={[showDescription ? styles.display : styles.hide]}>
+            <Text>Phone#: {placeDetails[index].formatted_phone_number}</Text>
+            <Text>{placeDetails[index].website}</Text>
+            <Text>
+              {placeDetails[index].opening_hours.weekday_text[0]}
+              {'\n'}
+              {placeDetails[index].opening_hours.weekday_text[1]}
+              {'\n'}
+              {placeDetails[index].opening_hours.weekday_text[2]}
+              {'\n'}
+              {placeDetails[index].opening_hours.weekday_text[3]}
+              {'\n'}
+              {placeDetails[index].opening_hours.weekday_text[4]}
+              {'\n'}
+              {placeDetails[index].opening_hours.weekday_text[5]}
+              {'\n'}
+              {placeDetails[index].opening_hours.weekday_text[6]}
+            </Text>
+            {placeDetails[index].reviews ? (
+              <FlatList
+                data={placeDetails[index].reviews}
+                keyExtractor={(item, i) => i.toString()}
+                renderItem={({ item }) => (
+                  <View>
+                    <Text>{item.author_name}</Text>
+                    <Text>Customer Rating: {item.rating}</Text>
+                    <Text>{item.relative_time_description}</Text>
+                    <Text>{item.text}</Text>
+                  </View>
+                )}
+              />
+            ) : (
+              <Text>No Reviews</Text>
+            )}
+          </View>
+        </View>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
   );
 };
+
+const styles = StyleSheet.create({
+  display: {
+    display: 'flex',
+  },
+  hide: {
+    display: 'none',
+  },
+});
 
 export default Description;
