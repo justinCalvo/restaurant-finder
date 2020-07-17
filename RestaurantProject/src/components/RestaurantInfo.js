5e20ac857db551d2f970b1365d61d1a8be51db6f
Justin Calvo <justin_calvo@hotmail.com>
2020-07-17 08:58:51 -0400

    Price level now respresented in dollar signs instead of a number

--- RestaurantProject/src/components/RestaurantInfo.js
+++ RestaurantProject/src/components/RestaurantInfo.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const RestaurantInfo = ({ restaurants, index }) => {
+  const [formatPriceLevel, setFormatPriceLevel] = useState('');
+
+  // TODO: how do we want to represent a price level of 0?
+  const formatPrice = useCallback(() => {
+    const priceLevel = restaurants[index].price_level;
+    if (priceLevel === 0) {
+      setFormatPriceLevel('0');
+    } else if (priceLevel === 1) {
+      setFormatPriceLevel('$');
+    } else if (priceLevel === 2) {
+      setFormatPriceLevel('$$');
+    } else if (priceLevel === 3) {
+      setFormatPriceLevel('$$$');
+    } else if (priceLevel === 4) {
+      setFormatPriceLevel('$$$$');
+    } else {
+      setFormatPriceLevel('0');
+    }
+  }, [restaurants, index]);
+
+  useEffect(() => {
+    formatPrice();
+  }, [formatPrice, restaurants]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.textName}>{restaurants[index].name}</Text>
@@ -9,7 +33,7 @@ const RestaurantInfo = ({ restaurants, index }) => {
         Address: {'\n'}
         {restaurants[index].formatted_address}
       </Text>
-      <Text>Price Level: {restaurants[index].price_level}</Text>
+      <Text>Price Level: {formatPriceLevel}</Text>
       <Text>Rating: {restaurants[index].rating}</Text>
     </View>
   );
