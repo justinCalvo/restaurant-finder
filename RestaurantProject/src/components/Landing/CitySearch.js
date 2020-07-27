27ac44357ccd192aa829e69b8243d4b712f4b784
Justin Calvo <justin_calvo@hotmail.com>
2020-07-27 15:50:11 -0400

    Moved API calls to helper file

--- RestaurantProject/src/components/Landing/CitySearch.js
+++ RestaurantProject/src/components/Landing/CitySearch.js
@@ -4,6 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import config from '../../../config';
 import { Routes } from '../../constants/NavConst';
+import { getRestaurants } from '../../API/getNearby';
 
 const CitySearch = ({ city, state, zipcode }) => {
   const navigation = useNavigation();
@@ -20,55 +21,14 @@ const CitySearch = ({ city, state, zipcode }) => {
       )
       .then(location => {
         const locationPath = location.data.results[0].geometry.location;
-        axios
-          .get(
-            `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurant&opennow&location=${
-              locationPath.lat
-            },${locationPath.lng}&radius=8046.72&type=restaurant&opennow&key=${
-              config.API_KEY
-            }`,
-          )
-          .then(data => {
-            let proximityResults = data.data.results;
-            setNextPageToken(data.data.next_page_token);
-            axios
-              .get(
-                `https://maps.googleapis.com/maps/api/place/details/json?place_id=${
-                  proximityResults[0].place_id
-                }&fields=formatted_phone_number,opening_hours/weekday_text,website,photo,review&key=${
-                  config.API_KEY
-                }`,
-              )
-              .then(description => {
-                axios
-                  .get(
-                    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${
-                      proximityResults[1].place_id
-                    }&fields=formatted_phone_number,opening_hours/weekday_text,website,photo,review&key=${
-                      config.API_KEY
-                    }`,
-                  )
-                  .then(newDescription => {
-                    for (var key in description.data.result) {
-                      proximityResults[0][key] = description.data.result[key];
-                    }
-                    for (var key in newDescription.data.result) {
-                      proximityResults[1][key] =
-                        newDescription.data.result[key];
-                    }
-                    setRestaurants(proximityResults);
-                  })
-                  .catch(err => {
-                    console.log(err);
-                  });
-              })
-              .catch(err => {
-                console.log(err);
-              });
-          })
-          .catch(err => {
-            console.log(err);
-          });
+        getRestaurants(
+          locationPath.lat,
+          locationPath.lng,
+          config,
+          axios,
+          setNextPageToken,
+          setRestaurants,
+        );
       })
       .catch(err => {
         console.log(err);
