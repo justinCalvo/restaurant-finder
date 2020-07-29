e3396676b7fba80c2ee2ea5e869c9c8b95c1405d
Justin Calvo <justin_calvo@hotmail.com>
2020-07-29 08:41:08 -0400

    Changed restaurant name size to 24 from 32

--- RestaurantProject/src/components/Details/Details.js
+++ RestaurantProject/src/components/Details/Details.js
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   restaurantName: {
-    fontSize: 32,
+    fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
   },
