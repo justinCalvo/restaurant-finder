d6ea01bb92bb42c8cf6a4bf05bc4e42bdb35c619
Justin Calvo <justin_calvo@hotmail.com>
2020-08-17 18:23:16 -0400

    Created a state of matches holding the restaurant and details data

--- RestaurantProject/src/components/Matches/Matches.js
+++ RestaurantProject/src/components/Matches/Matches.js
@@ -1,45 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import {
-  FlingGestureHandler,
-  Directions,
-  State,
-} from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
-import { Routes } from '../../constants/NavConst';
-import { useSelector } from 'react-redux';
-// import { getDetails } from '../../redux/actions/detailsActions';
 
-const Matches = () => {
-  // const dispatch = useDispatch();
-  const navigation = useNavigation();
-  const rest = useSelector(state => state.restaurants);
-  const deet = useSelector(state => state.details);
-
-  const openModal = () => {
-    navigation.navigate(Routes.MatchesModal);
-  };
-  // console.log('restaurante', rest);
-  // console.log('give me the ', deet);
-
-  // useEffect(() => {
-  //   if (state.details.length === 0) {
-  //     dispatch(getDetails([], state.restaurants, 0));
-  //   }
-  // }, [state.details]);
-
+const Matches = ({ displayMatches }) => {
+  console.log('this: ', displayMatches);
   return (
-    <FlingGestureHandler
-      direction={Directions.DOWN}
-      onHandlerStateChange={({ nativeEvent }) => {
-        if (nativeEvent.state === State.ACTIVE) {
-          openModal();
-        }
-      }}>
-      <View style={styles.container}>
-        <Text style={styles.banner}>lol</Text>
-      </View>
-    </FlingGestureHandler>
+    <View style={styles.container}>
+      <Text style={styles.banner}>lol</Text>
+    </View>
   );
 };
 
--- RestaurantProject/src/screens/Details/MatchesScreen.js
+++ RestaurantProject/src/screens/Details/MatchesScreen.js
@@ -1,24 +1,51 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Dimensions } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { setMatches } from '../../redux/actions/matchesActions';
 
+import Matches from '../../components/Matches/Matches';
+
 const MatchesScreen = ({ navigation }) => {
+  const [displayMatches, setDisplayMatches] = useState({});
+
+  const restaurants = useSelector(state => state.restaurants);
+  const details = useSelector(state => state.details);
   const matches = useSelector(state => state.matches);
   const dispatch = useDispatch();
-  console.log(matches.matches);
 
   useEffect(() => {
     const resetTabBarBadge = navigation.addListener('tabPress', e => {
       dispatch(setMatches(undefined, undefined, matches));
-    });
 
+      let item,
+        newMatches = [];
+
+      for (item in matches.matches) {
+        for (let i = 0; i < restaurants.restaurants.length; i++) {
+          if (restaurants.restaurants[i].place_id === item) {
+            newMatches.push({
+              formatted_phone_number: details.details[i].formatted_phone_number,
+              opening_hours: details.details[i].opening_hours,
+              photos: details.details[i].photos,
+              reviews: details.details[i].reviews,
+              website: details.details[i].website,
+              formatted_address: restaurants.restaurants[i].formatted_address,
+              name: restaurants.restaurants[i].name,
+              price_level: restaurants.restaurants[i].price_level,
+              rating: restaurants.restaurants[i].rating,
+              user_ratings_total: restaurants.restaurants[i].user_ratings_total,
+            });
+          }
+        }
+      }
+      setDisplayMatches(newMatches);
+    });
     return resetTabBarBadge;
-  }, [dispatch, matches, navigation]);
+  }, [details.details, dispatch, matches, navigation, restaurants.restaurants]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello</Text>
+      <Matches displayMatches={displayMatches} />
     </SafeAreaView>
   );
 };
