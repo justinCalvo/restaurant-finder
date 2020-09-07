9d50e353259f655f1496f22b2c4465ee4c0a0cb4
Justin Calvo <justin_calvo@hotmail.com>
2020-09-07 10:34:05 -0400

    Removed photos action and reducer, these files where not being used

--- RestaurantProject/src/redux/reducers/rootReducer.js
+++ RestaurantProject/src/redux/reducers/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import restaurantsReducer from './restaurantsReducer';
 import detailsReducer from './detailsReducer';
-import photosReducer from './photosReducer';
 import matchesReducer from './matchesReducer';
 
 const rootReducer = combineReducers({
   restaurants: restaurantsReducer,
   details: detailsReducer,
-  photos: photosReducer,
   matches: matchesReducer,
 });
 
